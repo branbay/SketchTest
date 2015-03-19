a55d01b17b68edfa8a05052d45b85c8a2ed8ceef
Brandon Bay <brandonbay2017@u.northwestern.edu>
2015-03-18 19:22:32 -0500

    All done

--- js/sketch.js
+++ js/sketch.js
@@ -1,21 +1,68 @@
 $(document).ready(function(){
-    
-    for (var i = 0; i < 16; i++) {
-    	for (var j = 0; j < 16; j++) {
-    		$('body').append('<div></div>')
-    	};
-    	$('body').append('</br>')
-    };
-    
+    var dim;
+    var h;
+    var s;
+    var value;
+   create();
+
+ });
+
+
+function create(){
+	
+	do{
+	h = parseInt(prompt("How many rows and collumns?","16"))
+
+	dim = (h>0 && h<70) ? h:'';
+	}while(!dim);
+
+	clear();
+
+	s = Math.round(660/dim);
+	//$(size);
+	
+	console.log("after size");
+	$('body').append('<table>');
+
+	for (var i = 0; i < dim; i++) {
+	 	$('table').append('<tr>');
+	};
+
+    for (var j = 0; j < dim; j++) {
+    	$('tr').append('<td><div></div></td>');
+	};
+
+
+	$('table').append('</tr>');
+
+	size(s);
+/*
+	$('div').mouseenter(function(){		//MUST REFRESH AFTER NEW CREATION
+		$(this).addClass('b');
+	})
+*/
+	$('div').mouseenter(function(){		//MUST REFRESH AFTER NEW CREATION
+		var opac = $(this).css('opacity');
+	
+		if (opac > 0)
+			//$(this).css('opacity',opac - 0.1);
+			$(this).fadeTo(200,opac - 0.1);
+		
+	})
+};
 
 
 
-$('div').mouseenter(function(){
+function size(){
+	console.log(s);
+	$("div").width(s);
+	$('div').height(s);
 
-	$(this).addClass('c');
-})
+};
 
+function clear(){
+	$('table').remove();
+};
 
-})
 
 //http://htmlpreview.github.io/?https://github.com/RebootJeff/myLearning/blob/master/odin.js.sketch_pad/index.html
